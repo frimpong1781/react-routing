ab321896222cfb0c25aea76ea5250ea37802b6b1
princess-hub <plinda.mensah@gmail.com>
2020-10-28 22:29:50 +0000

    Add Contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div>
+            <h1>Contact</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, aliquam quisquam totam 
+                impedit consequatur molestias nesciunt laudantium distinctio commodi. Necessitatibus 
+                velit nulla dolor beatae quidem eaque ex dignissimos laboriosam iste?
+            </p>
+        </div>
+    );
+}
+
+export default Contact;
