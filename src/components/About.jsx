41fc23d8f7e358b349e7f768e1bf8ecf3924759f
princess-hub <plinda.mensah@gmail.com>
2020-10-28 22:29:22 +0000

    Add About component

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, aliquam quisquam totam 
+                impedit consequatur molestias nesciunt laudantium distinctio commodi. Necessitatibus 
+                velit nulla dolor beatae quidem eaque ex dignissimos laboriosam iste?
+            </p>
+        </div>
+    );
+}
+
+export default About;
